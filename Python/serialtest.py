34571048526177af9e2c98eb6210c8cbdf503b41
DeveloperUchida <kouei28poiou@icloud.com>
2023-12-12 12:55:20 +0900

    DataUpdate 2023/12/12

--- Python/serialtest.py
+++ Python/serialtest.py
@@ -1,17 +1,19 @@
 import serial
 
-#  COMポートを開く
+# COMポートを開く
 print("Open Port")
 ser = serial.Serial("COM3", 9600)
-while True:
-    try:
-        command = input("type 0 or 1: ")
-        if command == "0" or command == "1":
-            ser.write(command.encode())
-        else:
-            print("ignore command")
-    except KeyboardInterrupt:
-        break
 
-print("Close Port")
-ser.close()
+try:
+    while True:
+        try:
+            command = input("type 0 or 1: ")
+            if command == "0" or command == "1":
+                ser.write(command.encode())
+            else:
+                print("Invalid command. Please enter 0 or 1.")
+        except KeyboardInterrupt:
+            break
+finally:
+    print("Close Port")
+    ser.close()
